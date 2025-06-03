52b98ee640f1fe058f80e4da0f99668902bef21f
Rio Knightley <rio.knightley@protonmail.com>
2025-06-03 23:53:16 +0100

    Update redux with thunks

--- src/store/searchSlice.ts
+++ src/store/searchSlice.ts
@@ -1,14 +1,35 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Post } from "@/types/post";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface SearchState {
   query: string;
   loading: boolean;
+  error: string;
+  results: Array<Post>;
 }
 
 const initialState: SearchState = {
   query: "",
   loading: false,
+  error: "",
+  results: [],
 };
+
+export const fetchResults = createAsyncThunk<Array<Post>, number>(
+  "search/fetchResults",
+  async (pageNumber) => {
+    console.log("fetching posts redux");
+
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_page=${pageNumber}`,
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch search results");
+    }
+    const data = await response.json();
+    return data;
+  },
+);
 
 const searchSlice = createSlice({
   name: "search",
@@ -20,11 +41,27 @@ const searchSlice = createSlice({
     setLoading(state, action: PayloadAction<boolean>) {
       state.loading = action.payload;
     },
-    resetSearch() {
-      return initialState;
+    setResults(state, action: PayloadAction<Array<Post>>) {
+      state.results = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchResults.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(fetchResults.fulfilled, (state, action) => {
+        state.loading = false;
+        state.results = [...state.results, ...action.payload];
+      })
+      .addCase(fetchResults.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "501 Internal Server Error";
+        state.results = [];
+      });
   },
 });
 
-export const { setQuery, setLoading, resetSearch } = searchSlice.actions;
+export const { setQuery, setLoading, setResults } = searchSlice.actions;
 export default searchSlice.reducer;
